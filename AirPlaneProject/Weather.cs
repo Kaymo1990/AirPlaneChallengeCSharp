05f0163b5f5f03e2adb767f45a257085f075f98d
Kay <khurrammohammed1990@gmail.com>
2019-10-07 20:05:45 +0100

    updated with random weather class

---
+++ AirPlaneProject/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirPlaneProject
+{
+    class Weather
+    {
+        public static bool IsStormy()
+        {
+            bool isStormy = Random.value
+                > 0.5f;
+        }
+    }
+}
