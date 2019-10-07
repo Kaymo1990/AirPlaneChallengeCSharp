4193eeff876aa7892c62ffb02fb5f81ca3abc8ed
Kay <khurrammohammed1990@gmail.com>
2019-10-07 16:42:05 +0100

    updated to have setup attribute for nunit

--- AirPlaneTests/AirPlaneTests.cs
+++ AirPlaneTests/AirPlaneTests.cs
@@ -3,44 +3,42 @@ using NUnit.Framework;
 
 namespace Tests
 {
-
+    [TestFixture]
     public class Tests
     {
-        Airplane plane = new Airplane();
+        Airplane airplane;
+        Airport airport;
 
-        [Test]
-        public void Land_ShouldReturnLanded_WhenCalled()
+        [SetUp] public void Init()
         {
-            Assert.AreEqual("Landed", plane.land());
+            airplane = new Airplane();
+            airport = new Airport("Test");
         }
 
-        [Test]
-        public void PlaneStatus_ShouldBeFlying_WhenCalled()
+
+        [Test] public void Land_ShouldReturnLanded_WhenCalled()
         {
-            var airplane = new Airplane();
+            Assert.AreEqual("Landed", airplane.land());
+        }
+
+        [Test] public void PlaneStatus_ShouldBeFlying_WhenCalled()
+        {
             Assert.AreEqual("Flying", airplane.PlaneStatus);
         }
 
-        [Test]
-        public void PlaneAirport_ShouldBeBlank_WhenFlying()
+        [Test] public void PlaneAirport_ShouldBeBlank_WhenFlying()
         {
-            var airplane = new Airplane();
             Assert.AreEqual("", airplane.CurrentAirport);
         }
 
-        [Test]
-        public void Airport_ShouldAcceptPlaneInHanger_AddPlaneToAirportHanger()
+        [Test] public void Airport_ShouldAcceptPlaneInHanger_AddPlaneToAirportHanger()
         {
-            var airPlane = new Airplane();
-            var airport = new Airport("Test");
-            airport.AcceptPlane(airPlane);
-            airport.AcceptPlane(airPlane);
+            airport.AcceptPlane(airplane);
+            airport.AcceptPlane(airplane);
             Assert.IsInstanceOf(typeof(Airplane), airport.planeHanger[1]);
         }
-        [Test]
-        public void Airport_ShouldHaveName_WhenInstantiated()
+        [Test] public void Airport_ShouldHaveName_WhenInstantiated()
         {
-            var airport = new Airport("Test");
             Assert.AreEqual("Test", airport.airportLocation);
         }
     }
